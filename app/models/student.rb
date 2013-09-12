09a3773d06a277f063692122ee583fcad241a786
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-11 19:54:28 -0700

    added Student#name and Student#age

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+	def name
+		return "#{first_name} #{last_name}"
+	end
+
+	def age
+		return Time.now.year - birthday.year
+	end
 end
