7b17a529b610f26de2bde68e9a97cef2aa4fdf20
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-11 21:14:38 -0700

    added Student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,12 +2,19 @@ require_relative '../../db/config'
 require 'date'
 
 class Student < ActiveRecord::Base
+	validates :email, format: { with: /.+[@]{1}.+[.]{1}.{2,}/, message: "Please enter a valid email address"}, uniqueness: true
+	validates :age, numericality: { greater_than: 5, message: "Must be older than 5 years of age"}
+	validates :phone_number_length_only_digits, numericality: {greater_than: 10, message: "Please enter a phone number with more than 10 digits"}
 
 	def name
 		return "#{first_name} #{last_name}"
 	end
 
 	def age
-		age = Time.now.year - birthday.year - (Time.now.yday < birthday.yday ? 1 : 0)
+		Time.now.year - birthday.year - (Time.now.yday < birthday.yday ? 1 : 0)
+	end
+
+	def phone_number_length_only_digits
+		phone.gsub(/[^0-9]/,'').length
 	end
 end
